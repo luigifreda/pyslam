124078e05868884a528bfa5161acd4c8e73a6296
Luigi <luigifreda@gmail.com>
2020-06-09 11:56:30 +0200

    update clean script

--- clean.sh
+++ clean.sh
@@ -15,4 +15,6 @@ print_blue "clearning thirdparty packages and utils..."
 
 rm -Rf thirdparty/pangolin 
 rm -Rf thirdparty/g2opy
-rm -Rf cpp/utils/build 
+rm -Rf thirdparty/protoc  # set by install_delf.sh 
+rm -Rf thirdparty/orbslam2_features/build
+rm -Rf cpp/utils/build  
