aef45fe35d39d44beb65414f27d7e937a63eecff
Luigi <luigifreda@gmail.com>
2021-05-30 18:53:58 +0200

    Fixes for Ubuntu 20 support

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -30,7 +30,7 @@ if ! pyenv install --list &> /dev/null; then
         echo "adding pyenv setting to bashrc"
         echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
         echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-        echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+        echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
     fi 
 
     source ~/.bashrc
